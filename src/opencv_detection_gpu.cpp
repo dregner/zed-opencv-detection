72ec9ffa15df65305043f668d5b2a44e1579ddd3
dregner <daniel_regner@hotmail.com>
2021-09-27 14:33:10 -0300

    Update cmake conflicts of OpenCV version

--- src/opencv_detection_gpu.cpp
+++ src/opencv_detection_gpu.cpp
@@ -51,14 +51,14 @@ int main(int argc, char **argv) {
     /// Initializing YOLO DETECTION
 
     // Load names of classes
-    std::string classesFile = "/home/nvidia/zed-opencv-detection/yolo_params/cfg/coco.names";
+    std::string classesFile = "/home/vant3d/zed-opencv-detection/yolo_params/cfg/coco.names";
     std::ifstream ifs(classesFile.c_str());
     std::string line;
     while (getline(ifs, line)) classes.push_back(line);
 
     // Give the configuration and weight files for the model
-    cv::String modelConfiguration = "/home/nvidia/zed-opencv-detection/yolo_params/cfg/yolov4.cfg";
-    cv::String modelWeights = "/home/nvidia/zed-opencv-detection/yolo_params/weights/yolov4.weights";
+    cv::String modelConfiguration = "/home/vant3d/zed-opencv-detection/yolo_params/cfg/yolov4.cfg";
+    cv::String modelWeights = "/home/vant3d/zed-opencv-detection/yolo_params/weights/yolov4.weights";
 
     // Load the network
     cv::dnn::Net net = cv::dnn::readNetFromDarknet(modelConfiguration, modelWeights);
